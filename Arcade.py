c95e2dd35eeaf677368511b505cf40a53dc12255
carl254917-netizen <carl25.4917@gmail.com>
2026-05-23 20:27:23 +0200

    Update Arcade.py

--- Arcade.py
+++ Arcade.py
@@ -91,7 +91,7 @@ def menu():
     root.configure(bg="black")
 
     window_width = 400
-    window_height = 320
+    window_height = 300
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x_position = (screen_width // 2) - (window_width // 2)
